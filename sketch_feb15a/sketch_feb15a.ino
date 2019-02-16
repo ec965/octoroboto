fca8311fcc7cc89d5cae1ab33b0cf882e53f2bc1
ec965 <enoch965@gmail.com>
2019-02-15 22:46:31 -0800

    sample code and motor functions
    added sample code and motor pins and functions

--- sketch_feb15a/sketch_feb15a.ino
+++ sketch_feb15a/sketch_feb15a.ino
@@ -10,6 +10,23 @@ long distance; /*variable for recording distance*/
 //set up buzzer
 int buzzerPin = 13;
 
+//set up M1 and M2 (SN754410..)
+//M1
+int motorEN1 = 5;
+int motorForward1 = 4;
+int motorReverse1 = 2;
+//M2
+int motorEN2 = 3;
+int motorForward2 = 1;
+int motorReverse2 = 0;
+int speed = 0;
+void forward(int);/*makes motor spin forward*/
+void reverse(int);/*makes motor spin backwards*/
+void off(int);/*turns motors off*/
+//set up M3(L293D)
+
+
+/*main code*/
 void setup() {
    Serial.begin(9600); /*starting serial monitor*/
    delay(1000);
@@ -21,6 +38,38 @@ void loop() {
    Serial.print(a);
    Serial.println("cm");
    delay(1000);
-  
-
 }
+
+
+//M1 and M2 code
+void forward(int speed){
+   Serial.println("forward");
+   speed = 255;
+   analogWrite(motorEN1, speed);
+   digitalWrite(motorForward1, HIGH);
+   digitalWrite(motorReverse1, LOW);
+   analogWrite(motorEN2, speed);
+   digitalWrite(motorForward2, HIGH);
+   digitalWrite(motorReverse2, LOW);
+}
+
+void reverse(int speed){
+  Serial.println("reverse");
+  speed = 255;
+  analogWrite(motorEN1, speed);
+  digitalWrite(motorForward1, LOW);
+  digitalWrite(motorReverse1, HIGH);
+  analogWrite(motorEN2, speed);
+  digitalWrite(motorForward2, LOW);
+  digitalWrite(motorReverse2, HIGH);
+}
+
+void off(int speed){
+  Serial.println("off");
+  speed = 0;
+  analogWrite(motorEN1,speed);
+  digitalWrite(motorForward1, LOW);
+  digitalWrite(motorReverse1, LOW);
+  analogWrite(motorEN2,speed);
+  digitalWrite(motorForward2, LOW);
+  digitalWrite(motorReverse2, LOW);
