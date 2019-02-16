a737a648dc61e93aaf52aa936d840149aab827dd
ec965 <enoch965@gmail.com>
2019-02-15 22:35:43 -0800

    pins for buzzer and ultrasonic sensor
    defined pins and added sample code for ultrasonic sensor

--- sketch_feb15a/sketch_feb15a.ino
+++ sketch_feb15a/sketch_feb15a.ino
@@ -1,9 +1,26 @@
+/*HACK UCI 2019*/
+
+/*setting up ultrasonic sensor*/
+#include "SR04.h"
+int TRIG_PIN = 12;
+int ECHO_PIN = 11;
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long distance; /*variable for recording distance*/
+
+//set up buzzer
+int buzzerPin = 13;
+
 void setup() {
-  // put your setup code here, to run once:
+   Serial.begin(9600); /*starting serial monitor*/
+   delay(1000);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+   distance = sr04.Distance();
+   Serial.print(a);
+   Serial.println("cm");
+   delay(1000);
+  
 
 }
